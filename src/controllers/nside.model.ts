ff10d9a605b30b9e0d3c92a702668094b2b1ec74
Tamás Róbert <t2000ro09bi28@gmail.com>
2022-05-24 21:49:31 +0200

    init

---
+++ src/controllers/nside.model.ts
@@ -0,0 +1,67 @@
+import { Schema, model } from "mongoose";
+
+const nsideSchema = new Schema(
+    {
+        _id: Number,
+        FK_neve: {
+            ref: "oneside",
+            type: Number,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        isGlutenFree: {
+            type: Boolean,
+            required: true,
+        },
+        prepTime: {
+            type: Number,
+            required: true,
+        },
+        minMaxExample: {
+            type: Number,
+            min: [1, "Too few stars, got {VALUE}"],
+            max: [5, "Too many stars, got {VALUE}"],
+            required: [true, "minMaxExample field is required"],
+        },
+        enumExample: {
+            type: String,
+            enum: {
+                values: ["Coffee", "Tea"],
+                message: "{VALUE} is not supported",
+            },
+        },
+        customValidatorExample: {
+            type: Number,
+            validate: {
+                validator: function (v: number) {
+                    return v % 2 == 0;
+                },
+                message: "Nem páros számot adott meg!",
+            },
+        },
+        dateExample: {
+            type: Date,
+            default: new Date(),
+            max: ["2100-12-31", "Csak 21. századi dátumot adhat meg!"],
+            validate: {
+                validator: function (v: Date) {
+                    return v >= new Date();
+                },
+                message: "Az aktuális dátumnál nem adhat meg korábbi dátumot!",
+            },
+        },
+    },
+    { versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+
+const nsideModel = model("nside", nsideSchema, "TáblaNeveN");
+
+export default nsideModel;
---
+++ src/controllers/oneside.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const onesideSchema = new Schema(
+    {
+        _id: Number,
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+    },
+    { versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+
+const onesideModel = model("oneside", onesideSchema, "TáblaNeve1");
+
+export default onesideModel;
